1cb3341ffdca770d4c63f5aefa28695a9b9cf51b
20017435 <emeric.lesage@adeo.com>
2026-04-15 11:35:59 +0200

    created TU for Salle entity

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,6 @@
 import pytest
 from core.models import Adherent
+from core.models import Salle
 
 @pytest.fixture
 # Crée un adhérent pour les tests
@@ -13,3 +14,20 @@ def test_est_abonne(adherent_test):
 def test_solde_restant(adherent_test):
     # On vérifie le solde restant de l'adhérent
     assert adherent_test.solde_restant == 50
+
+@pytest.fixture
+# Créé une salle de tennis pour 4 personnes
+def salle_test():
+    return Salle("Tennis", 4)
+
+def test_salle_type(salle_test):
+    # On vérifie que la salle est bien de type "Tennis"
+    assert salle_test.type == "Tennis"
+
+def test_salle_capacite(salle_test):
+    # On vérifie que la salle est bien de capacité 4
+    assert salle_test.capacite == 4
+
+def test_places_restantes(salle_test):
+    # On vérifie que le nombre de places restantes est égal à la capacité initiale
+    assert salle_test.places_restantes == 4
