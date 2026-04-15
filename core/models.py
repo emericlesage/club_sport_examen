a58812624740e3c5472c210e96311ae6b791ddec
20017435 <emeric.lesage@adeo.com>
2026-04-15 11:40:07 +0200

    created methods for Salle entity

--- core/models.py
+++ core/models.py
@@ -12,3 +12,22 @@ class Adherent:
     def solde_restant(self):
         # Retourne le solde restant de l'adhérent
         return self.solde
+    
+class Salle:
+    def __init__(self, nom_type, capacite):
+        self._type = nom_type
+        self._capacite = capacite
+    
+    @property
+    def type(self):
+        return self._type
+    
+    @property
+    def capacite(self):
+        return self._capacite
+    
+    @property
+    def places_restantes(self):
+        # Retourne le nombre de places restantes dans la salle
+        # Methode qui va evoluer plus tard pour prendre en compte les réservations
+        return self.capacite
